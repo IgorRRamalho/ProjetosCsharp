6449e43aa2fa005702b3622748fb10dc0801c839
Igor <pacfa_sky@hotmail.com>
2023-09-27 15:47:50 -0400

    pri commit

---
+++ Projeto01/Projeto01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            void Imprimir()
+            {
+                Console.WriteLine("Olá mundo");
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ReadLine();
+                
+            }
+
+            Imprimir();
+
+            
+        }
+    }
+}
