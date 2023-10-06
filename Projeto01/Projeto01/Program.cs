bb1f2b58ca1830bd3c2e08138ba4570d0ccfa80b
Igor <pacfa_sky@hotmail.com>
2023-10-05 21:53:58 -0400

    commir

--- Projeto01/Projeto01/Program.cs
+++ Projeto01/Projeto01/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Projeto01
 {
@@ -10,18 +8,109 @@ namespace Projeto01
     {
         static void Main(string[] args)
         {
-
-            void Imprimir()
-            {
-                Console.WriteLine("Olá mundo");
-                Console.BackgroundColor = ConsoleColor.Blue;
-                Console.ReadLine();
-                
-            }
-
-            Imprimir();
+            float resul;
+            double result;
+            int op;
 
             
+            void Subtra()
+            {
+                resul = Valor1() - Valor2();
+                Console.WriteLine("O RESULTADO DA SUBTRAÇÃO FOI :" + resul);
+                Menu();
+            }
+            
+            void Mult()
+            {
+                resul = Valor1() * Valor2();
+                Console.WriteLine("O RESULTADO DA MULTIPLACAÇÃO FOI :" + resul);
+
+                Menu();
+            }
+
+            void Div()
+            {
+   
+                result = Convert.ToDouble(Valor1())/ Convert.ToDouble(Valor2());
+                Console.WriteLine("O RESULTADO DA DIVISÃO FOI :" + result);
+
+                Menu();  
+            }
+            
+            void Soma()
+            { 
+                resul = Valor1() + Valor2();
+                Console.WriteLine("O RESULTADO DA SOMA FOI :" + resul);
+
+                Menu();
+            }
+
+            int Valor2()
+            {
+                int valor2;
+                Console.WriteLine("Digite o segundo valor ->");
+                valor2 = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                return valor2;
+                
+
+            }
+
+            int Valor1()
+            {
+                int valor1;
+                Console.WriteLine("Digite o primeiro valor ->");
+                valor1 = Convert.ToInt32(Console.ReadLine());
+                return valor1;
+
+            }
+
+            void Menu()
+            {
+                Console.WriteLine("-----------------MENU-----------------");
+                Console.WriteLine("\n(1)Soma\t(2)Subtrair\n(3)Dividir\t(4)Multiplicar\n(-1)Sair");
+                Console.WriteLine("OPÇÃO ->");
+                op = Convert.ToInt32(Console.ReadLine());
+
+                switch (op)
+                {
+                    case 1:
+                        Soma();
+                        break;
+
+                    case 2:
+                        Subtra();
+                        break;
+
+                    case 3:
+                        Div();
+                        break;
+                    case 4:
+                        Mult();
+                        break;
+
+                    case -1:
+                        break;
+
+                    default:
+                        Console.WriteLine("*OPÇÃO INVÁLIDA");
+                        Menu();
+                        break;
+
+                }
+                      
+                
+
+                
+
+
+            }
+
+            Menu();
+
+
+
         }
+
     }
 }
